8fc3cfaadae29a175d3950f03f0ce632b32281e8
Eric Ashmore <edashmore@gmail.com>
2016-02-15 17:44:45 -0800

    feature(sql_object): can return list of columns

--- sql_object.rb
+++ sql_object.rb
@@ -2,6 +2,20 @@ require_relative 'db_connection'
 require 'active_support/inflector'
 
 class SQLObject
+  def self.columns
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    cols[0].map!(&:to_sym)
+    @columns = cols[0]
+  end
+
   def self.table_name=(table_name)
     @table_name = table_name
   end
